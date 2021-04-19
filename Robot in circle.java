4cc6cd4a368c34f2ca3c5421245f44ff10af500f
Kapish Garg <kapish5467@gmail.com>
2021-04-19 10:38:20 -0700

    Completed

---
+++ Robot in circle.java
@@ -0,0 +1,18 @@
+//time complexity-O(n)
+//Space complexity-O(1)
+//Ran on leetcode-Yes
+//Solution with comments-
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int x = 0, y = 0, i = 0, d[][] = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
+        for (int j = 0; j < instructions.length(); ++j)
+            if (instructions.charAt(j) == 'R')
+                i = (i + 1) % 4;
+            else if (instructions.charAt(j) == 'L')
+                i = (i + 3) % 4;
+            else {
+                x += d[i][0]; y += d[i][1];
+            }
+        return x == 0 && y == 0 || i > 0;//if we are starting point or are not facing north
+    }
+}
