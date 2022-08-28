d872c9a9f205d4d5e389f3401d8cccfc066123be
Hanuman Reddy <39452135+HanumanReddy1212@users.noreply.github.com>
2022-08-28 00:18:31 -0700

    array-5 done

---
+++ calculateTax.java
@@ -0,0 +1,21 @@
+//Tc - O(n)
+//Sc - O(1)
+class Solution {
+    public double calculateTax(int[][] brackets, int income) {
+        double left = income;
+        double limit = 0;
+        int i = 0;
+        double tax = 0;
+
+        while(left > 0){
+            int[] bracket = brackets[i];
+            double percentage = bracket[1];
+            double taxable = Math.min(left , bracket[0] - limit);
+            tax += taxable * (percentage / 100.00);
+            left -= taxable;
+            limit = bracket[0];
+            i++;
+        }
+        return tax;
+    }
+}
