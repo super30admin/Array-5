b787f2030aec43a82e10cf37812ef13047fd9526
Mani9333 <90652612+Mani9333@users.noreply.github.com>
2022-09-27 11:43:58 -0700

    Create CalcTax.java

---
+++ CalcTax.java
@@ -0,0 +1,35 @@
+
+public class Main {
+    public static double calculatetax(List<List<Double>> taxslabs, double salary){
+        double left = salary;
+        double previous = 0;
+        double taxable = 0;
+        double tax = 0;
+        
+        int i = 0;
+        while(left>0){
+            List<Double> taxslab = taxslabs.get(i);
+            double taxrate = taxslab.get(1);
+            if(taxslab.get(0) == null){
+                tax += left*taxrate;
+                return tax;
+            }
+            taxable = Math.min(taxslab.get(0) - previous, left);
+            tax += taxable * taxrate;
+            left -= taxable;
+            previous = taxslab.get(0);
+            i++;
+        }
+        return tax;
+    }
+    
+    public static void main(String[] args) {
+        List<List<Double>> taxslabs = new ArrayList<>();
+        taxslabs.add(Arrays.asList(10000.0,0.1));
+        taxslabs.add(Arrays.asList(20000.0,0.2));
+        taxslabs.add(Arrays.asList(30000.0,0.3));
+        taxslabs.add(Arrays.asList(null,0.4));
+        double tax = calculatetax(taxslabs, 45000);
+        System.out.println(tax);
+    }
+}
