58548f9bed63e3db7484ee441a88133b150fa4c1
tirthaj.rikame@gmail.com <tirthaj.rikame@gmail.com>
2023-06-08 14:46:17 -0400

    Completed Array-5. Please review.

--- Calculate Tax.py
+++ Calculate Tax.py
@@ -1,18 +1,33 @@
 import collections
-bracket = [[10000,0.3],[20000,0.2],[30000,0.1],[None,0.1]]
-salary = 80000
+# bracket = [[10000,0.3],[20000,0.2],[30000,0.1],[None,0.1]]
+bracket = [[3,0.5],[7,0.1],[12,0.25]]
+salary = 10
 tax = 0
 dq = collections.deque()
 dq.append(salary)
 i = 0
+prev = 0
 while dq:
     curr = dq.popleft()
     taxable, per = bracket[i]
-    if taxable == None or (curr-taxable) < 0:
+    if taxable == None:
         # taxable = curr
         # curr = 0
         tax += (curr * per)
+        curr -= curr
+        print("Tax at ", i, tax, curr)
         break
+
+    taxable = taxable - prev
+    prev = bracket[i][0]
+    if (curr - taxable) <= 0:
+        # taxable = curr
+        # curr = 0
+        tax += (curr * per)
+        print("Tax at ", i, tax, curr)
+        break
+    # per = per / 100
+
     tax += (taxable * per)
     curr -= taxable
     print("Tax at ", i, tax, curr)
